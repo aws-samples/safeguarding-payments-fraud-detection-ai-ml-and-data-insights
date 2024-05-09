6c90638db388a3a6eb0b071603be0e670df97bf7
Eugene Istrati <eistrati@amazon.com>
2024-05-09 19:14:36 -0400

    core components

--- iac/core/vpc_endpoint/locals.tf
+++ iac/core/vpc_endpoint/locals.tf
@@ -4,4 +4,9 @@
 locals {
   gateways   = split(",", element(split(":", try(var.vpce_mapping, ":")), 0))
   interfaces = split(",", element(split(":", try(var.vpce_mapping, ":")), 1))
+  subnet_ids = (
+    data.terraform_remote_state.subnet.outputs.nat_subnet_ids != [] ?
+    data.terraform_remote_state.subnet.outputs.nat_subnet_ids :
+    data.terraform_remote_state.subnet.outputs.igw_subnet_ids
+  )
 }
