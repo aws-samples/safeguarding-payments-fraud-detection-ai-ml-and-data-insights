26ae09884cd05f68696c880460281275ee1cfd72
Eugene Istrati <eistrati@amazon.com>
2024-05-14 12:17:53 -0400

    multi-platform support

--- iac/cicd/codebuild_project/locals.tf
+++ iac/cicd/codebuild_project/locals.tf
@@ -44,5 +44,75 @@ locals {
       type  = "PLAINTEXT"
       value = local.spf_gid
     },
+    {
+      name  = "SPF_DIR"
+      type  = "PLAINTEXT"
+      value = "iac/core"
+    },
+    {
+      name  = "SPF_VPC_ID"
+      type  = "PLAINTEXT"
+      value = ""
+    },
+    {
+      name  = "SPF_VPCE_MAPPING"
+      type  = "PLAINTEXT"
+      value = ""
+    },
+    {
+      name  = "SPF_SUBNETS_IGW_CREATE"
+      type  = "PLAINTEXT"
+      value = "false"
+    },
+    {
+      name  = "SPF_SUBNETS_IGW_MAPPING"
+      type  = "PLAINTEXT"
+      value = ""
+    },
+    {
+      name  = "SPF_SUBNETS_NAT_CREATE"
+      type  = "PLAINTEXT"
+      value = "false"
+    },
+    {
+      name  = "SPF_SUBNETS_NAT_MAPPING"
+      type  = "PLAINTEXT"
+      value = ""
+    },
+    {
+      name  = "SPF_SUBNETS_CAGW_CREATE"
+      type  = "PLAINTEXT"
+      value = "false"
+    },
+    {
+      name  = "SPF_SUBNETS_CAGW_MAPPING"
+      type  = "PLAINTEXT"
+      value = ""
+    },
+    {
+      name  = "SPF_EKS_CLUSTER_NAME"
+      type  = "PLAINTEXT"
+      value = format("spf-eks-cluster-%s-%s", data.aws_region.this.name, local.spf_gid)
+    },
+    {
+      name  = "SPF_EKS_NODE_TYPE"
+      type  = "PLAINTEXT"
+      value = "fargate"
+    },
+    {
+      name  = "SPF_EKS_NODE_ARCH"
+      type  = "PLAINTEXT"
+      value = "x86"
+    },
+    {
+      name  = "SPF_EKS_NODE_EC2"
+      type  = "PLAINTEXT"
+      value = ""
+    },
+    {
+      name  = "SPF_EKS_NODE_EBS"
+      type  = "PLAINTEXT"
+      value = ""
+    },
   ]
 }
