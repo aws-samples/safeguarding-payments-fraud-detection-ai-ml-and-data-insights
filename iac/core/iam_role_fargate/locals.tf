434ace55e082ece2f252880f84f9a36f7bd0719d
Eugene Istrati <eistrati@amazon.com>
2024-05-15 17:17:51 -0400

    allow more privileges

--- iac/core/iam_role_fargate/locals.tf
+++ iac/core/iam_role_fargate/locals.tf
@@ -4,7 +4,8 @@
 locals {
   spf_gid = (var.spf_gid == null ? data.terraform_remote_state.s3.outputs.spf_gid : var.spf_gid)
   policies = [
-    "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy",
     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+    "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
   ]
 }
--- iac/core/iam_role_kubernetes/locals.tf
+++ iac/core/iam_role_kubernetes/locals.tf
@@ -5,6 +5,8 @@ locals {
   spf_gid = (var.spf_gid == null ? data.terraform_remote_state.s3.outputs.spf_gid : var.spf_gid)
   suffix = format("%s-%s", data.aws_region.this.name, local.spf_gid)
   policies = [
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
     "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess",
     "arn:aws:iam::aws:policy/AmazonS3FullAccess",
     "arn:aws:iam::aws:policy/AmazonSSMFullAccess",
