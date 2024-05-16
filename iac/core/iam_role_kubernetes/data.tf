91bb0d25210d2cc142fcb871973e8f58c6f250b9
Eugene Istrati <eistrati@amazon.com>
2024-05-16 10:45:58 -0400

    oidc provider

--- iac/core/iam_role_kubernetes/data.tf
+++ iac/core/iam_role_kubernetes/data.tf
@@ -14,7 +14,7 @@ data "aws_iam_policy_document" "role" {
     condition {
       test     = "StringEquals"
       variable = format("%s:aud", replace(data.terraform_remote_state.eks.outputs.oidc_provider_url, "https://", ""))
-      values   = "sts.amazonaws.com"
+      values   = ["sts.amazonaws.com"]
     }
 
     condition {
