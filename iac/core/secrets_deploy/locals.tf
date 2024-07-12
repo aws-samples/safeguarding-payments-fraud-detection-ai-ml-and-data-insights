c2a6a08c1bbe830024873f1d51d9c991f3213f4a
Eugene Istrati <eistrati@amazon.com>
2024-07-12 18:04:52 -0400

    add missing bucket name

--- iac/core/secrets_deploy/locals.tf
+++ iac/core/secrets_deploy/locals.tf
@@ -13,5 +13,6 @@ locals {
     SPF_SERVICE_DBNAME    = var.q.srvname
     SPF_SERVICE_NAMESPACE = format("%s-%s-%s", var.q.srvprefix, data.aws_region.this.name, local.spf_gid)
     SPF_SERVICE_ROLE      = data.terraform_remote_state.iam.outputs.arn
+    SPF_S3_BUCKET         = data.terraform_remote_state.s3.outputs.id
   }
 }
