0b663f3de82cc738142f094ffc01dd69a5a04457
Eugene Istrati <eistrati@amazon.com>
2024-07-18 18:26:43 -0400

    architecture improvements

--- iac/core/s3_runtime/main.tf
+++ iac/core/s3_runtime/main.tf
@@ -72,7 +72,7 @@ resource "random_id" "this" {
 # Adding guidance solution ID via AWS CloudFormation resource
 #--------------------------------------------------------------
 resource "aws_cloudformation_stack" "this" {
-    name = "spf-tracking-stack"
+    name = format("spf-tracking-stack-%s-%s", data.aws_region.this.name, local.spf_gid)
     template_body = <<STACK
     {
         "AWSTemplateFormatVersion": "2010-09-09",
