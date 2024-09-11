6ac4fc648f5c3728fbdc21ba69dad7df26aeadf4
eistrati <eistrati@users.noreply.github.com>
2024-09-11 12:38:05 -0400

    data collector improvements (#40)

--- app/data-collector/src/main/java/aws/sample/paymentfraud/collector/CollectorConfig.java
+++ app/data-collector/src/main/java/aws/sample/paymentfraud/collector/CollectorConfig.java
@@ -2,7 +2,7 @@ package aws.sample.paymentfraud.collector;
 
 import java.util.logging.Logger;
 
-public class ConfigMapCollectorConfig {
+public class CollectorConfig {
 
     public enum Configs {
         FILE_DATA_EXTRACT_FOLDER_IN_S3,
@@ -12,19 +12,18 @@ public class ConfigMapCollectorConfig {
         PAYMENT_DATA_FILE,
         REGION,
         S3_BUCKET,
-        S3_FOLDER_COLLECTOR,
+        COLLECTOR_STATE,
+        COLLECTOR_FOLDER,
         MINIO_HOST,
         MINIO_USERNAME,
         MINIO_PASSWORD
     }
 
-    private final static Logger LOGGER = Logger.getLogger(ConfigMapCollectorConfig.class.getName());
+    public static final String COLLECTOR_STATE_START_LINE_NUMBER = "startLineNumber";
 
-    public boolean updateParameter(String parameterName, String parameterValue) {
-        return false;
-    }
+    private final static Logger LOGGER = Logger.getLogger(CollectorConfig.class.getName());
 
-    public ConfigMapCollectorConfig() {
+    public CollectorConfig() {
         LOGGER.info("FILE_DATA_EXTRACT_FOLDER_IN_S3 - " + System.getenv("FILE_DATA_EXTRACT_FOLDER_IN_S3"));
         LOGGER.info("FILE_LINE_NUMBER_TO_READ_FROM - " + System.getenv("FILE_LINE_NUMBER_TO_READ_FROM"));
         LOGGER.info("FILES_LOCATION_IN_S3 - " + System.getenv("FILES_LOCATION_IN_S3"));
@@ -32,20 +31,24 @@ public class ConfigMapCollectorConfig {
         LOGGER.info("PAYMENT_DATA_FILE - " + System.getenv("PAYMENT_DATA_FILE"));
         LOGGER.info("REGION - " + System.getenv("REGION"));
         LOGGER.info("S3_BUCKET - " + System.getenv("S3_BUCKET"));
-        LOGGER.info("S3_FOLDER_COLLECTOR - " + System.getenv("S3_FOLDER_COLLECTOR"));
+        LOGGER.info("COLLECTOR_STATE - " + System.getenv("COLLECTOR_STATE"));
+        LOGGER.info("COLLECTOR_FOLDER - " + System.getenv("COLLECTOR_FOLDER"));
         LOGGER.info("MINIO_HOST - " + System.getenv("MINIO_HOST"));
         LOGGER.info("MINIO_USERNAME - " + System.getenv("MINIO_USERNAME"));
         LOGGER.info("MINIO_PASSWORD - " + System.getenv("MINIO_PASSWORD"));
     }
 
-    public String getConfig(ConfigMapCollectorConfig.Configs key) {
+    public String getConfig(CollectorConfig.Configs key) {
         /* if(key.equals(Configs.FILE_DATA_EXTRACT_FOLDER_IN_S3)) return "file_extract";
-        else if(key.equals(Configs.FILE_LINE_NUMBER_TO_READ_FROM)) return "1";
+        else if(key.equals(Configs.FILE_LINE_NUMBER_TO_READ_FROM)) return "50001";
         else if (key.equals(Configs.FILES_LOCATION_IN_S3)) return "raw_payment_request_files";
         else if (key.equals(Configs.MAX_LINES_TO_READ)) return "10000";
         else if (key.equals(Configs.PAYMENT_DATA_FILE)) return "transaction_data_100K_full.csv";
         else if (key.equals(Configs.S3_BUCKET)) return "payment-fraud-detection-app-us-east-1";
-        else if (key.equals(Configs.MINIO_HOST)) return "http://k8s-paymentf-minioalb-859512b7f4-d42861e90e314b53.elb.us-east-1.amazonaws.com:9000/";
+        else if (key.equals(Configs.REGION)) return "us-east-1";
+        else if (key.equals(Configs.COLLECTOR_STATE)) return "collector-state.json";
+        else if (key.equals(Configs.COLLECTOR_FOLDER)) return "data-collector";
+        else if (key.equals(Configs.MINIO_HOST)) return "";//"http://k8s-paymentf-minioalb-859512b7f4-d42861e90e314b53.elb.us-east-1.amazonaws.com:9000/";
         else if (key.equals(Configs.MINIO_USERNAME)) return "minioadmin";
         else if (key.equals(Configs.MINIO_PASSWORD)) return "minioadmin";
         return null; */
--- app/data-collector/src/main/java/aws/sample/paymentfraud/collector/StorageService.java
+++ app/data-collector/src/main/java/aws/sample/paymentfraud/collector/StorageService.java
@@ -1,33 +1,42 @@
 package aws.sample.paymentfraud.collector;
 
+import java.io.IOException;
 import java.net.URI;
 import java.text.DecimalFormat;
 import java.util.Calendar;
 import java.util.TimeZone;
 import java.util.logging.Logger;
 
+import org.json.JSONObject;
+
 import com.amazonaws.util.StringUtils;
 
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
+import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
+import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 public class StorageService {
 
     private final static Logger LOGGER = Logger.getLogger(StorageService.class.getName());
 
-    ConfigMapCollectorConfig collectorConfig = new ConfigMapCollectorConfig();
+    CollectorConfig collectorConfig = new CollectorConfig();
 
     private S3Client s3 = null;
 
     public StorageService() throws Exception {
-        String minioHost = getCollectorConfig().getConfig(ConfigMapCollectorConfig.Configs.MINIO_HOST);//System.getenv("MINIO_HOST");
+        String minioHost = getCollectorConfig().getConfig(CollectorConfig.Configs.MINIO_HOST);//System.getenv("MINIO_HOST");
         LOGGER.info("MINIO HOST is - " + minioHost);
         if (StringUtils.isNullOrEmpty(minioHost)) {
-            s3 = S3Client.builder().region(Region.of(getCollectorConfig().getConfig(ConfigMapCollectorConfig.Configs.REGION))).build();
+            s3 = S3Client.builder().region(Region.of(getCollectorConfig().getConfig(CollectorConfig.Configs.REGION))).build();
             LOGGER.info("Initialized S3 for storage");
         } else {
             s3 = S3Client.builder()
@@ -35,13 +44,12 @@ public class StorageService {
                     .endpointOverride(URI.create(minioHost))
                     .credentialsProvider(
                             StaticCredentialsProvider.create(AwsBasicCredentials.create(
-                                getCollectorConfig().getConfig(ConfigMapCollectorConfig.Configs.MINIO_USERNAME),
-                                getCollectorConfig().getConfig(ConfigMapCollectorConfig.Configs.MINIO_PASSWORD))))
-                    .region(Region.of(ConfigMapCollectorConfig.Configs.REGION.name())) // You can use any region here
+                                getCollectorConfig().getConfig(CollectorConfig.Configs.MINIO_USERNAME),
+                                getCollectorConfig().getConfig(CollectorConfig.Configs.MINIO_PASSWORD))))
+                    .region(Region.of(CollectorConfig.Configs.REGION.name())) // You can use any region here
                     .build();
             LOGGER.info("Initialized MinIO for storage");
         }
-
     }
 
     public String storeFile(String data) {
@@ -55,7 +63,7 @@ public class StorageService {
         String second = mFormat.format(calendar.get(Calendar.SECOND));
 
         String objectKey = new StringBuilder()
-                .append(getCollectorConfig().getConfig(ConfigMapCollectorConfig.Configs.S3_FOLDER_COLLECTOR))
+                .append(getCollectorConfig().getConfig(CollectorConfig.Configs.COLLECTOR_FOLDER))
                 .append("/")
                 .append(year).append("/")
                 .append(month).append("/")
@@ -68,6 +76,38 @@ public class StorageService {
         return location;
     }
 
+    private byte[] readStateFileFromStorage(String stateFileName) throws IOException {
+
+        HeadObjectRequest headObjectRequest = HeadObjectRequest.builder()
+                .bucket(getCollectorConfig().getConfig(CollectorConfig.Configs.S3_BUCKET))
+                .key(stateFileName)
+                .build();
+
+        try {
+            HeadObjectResponse response = s3.headObject(headObjectRequest); // if no exception is thrown that means the file exists
+            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                    .bucket(getCollectorConfig().getConfig(CollectorConfig.Configs.S3_BUCKET))
+                    .key(stateFileName)
+                    .build();
+
+            ResponseInputStream<GetObjectResponse> stateResponse = s3.getObject(getObjectRequest);
+            return stateResponse.readAllBytes();
+        } catch (NoSuchKeyException e) {
+            return null; // Object does not exist
+        }
+        catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public JSONObject getCollectorState() throws IOException {
+        byte[] responseBytes = readStateFileFromStorage(getCollectorConfig().getConfig(CollectorConfig.Configs.COLLECTOR_STATE));
+        if (responseBytes == null) return null;
+
+        String fileContent = new String(responseBytes);
+        return new JSONObject(fileContent);
+    }
+
     public String store(String data, String objectName) {
         if (StringUtils.isNullOrEmpty(data)) {
             LOGGER.info("No data to store for name " + objectName);
@@ -75,14 +115,14 @@ public class StorageService {
         }
 
         PutObjectRequest request = PutObjectRequest.builder()
-                .bucket(getCollectorConfig().getConfig(ConfigMapCollectorConfig.Configs.S3_BUCKET))
+                .bucket(getCollectorConfig().getConfig(CollectorConfig.Configs.S3_BUCKET))
                 .key(objectName)
                 .build();
         s3.putObject(request, RequestBody.fromBytes(data.getBytes()));
-        return getCollectorConfig().getConfig(ConfigMapCollectorConfig.Configs.S3_BUCKET) + "/" + objectName;
+        return getCollectorConfig().getConfig(CollectorConfig.Configs.S3_BUCKET) + "/" + objectName;
     }
 
-    private ConfigMapCollectorConfig getCollectorConfig() {
+    private CollectorConfig getCollectorConfig() {
         return collectorConfig;
     }
 
