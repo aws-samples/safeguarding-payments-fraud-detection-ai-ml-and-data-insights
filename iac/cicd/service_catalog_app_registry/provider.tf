91d08248045d7d1e6108f083ec566911c20f2b29
Eugene Istrati <eistrati@amazon.com>
2025-08-11 09:02:05 -0400

    version upgrade

--- iac/cicd/service_catalog_app_registry/provider.tf
+++ iac/cicd/service_catalog_app_registry/provider.tf
@@ -21,7 +21,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.8.0"
+      version = "5.95.0"
     }
   }
 }
--- iac/global.tf
+++ iac/global.tf
@@ -26,7 +26,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.8.0"
+      version = "5.95.0"
     }
   }
 }
